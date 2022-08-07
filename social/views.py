a85f586cc88c714510d9d37f4afe9308d2402df7
Robert Coffie <rcoffie22@yhahoo.com>
2022-08-07 12:41:40 -0700

    working on comment model

--- social/views.py
+++ social/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from social.models import Post
-from social.serializers import PostSerializer
+from social.models import Post, Comment
+from social.serializers import PostSerializer, CommentSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -48,5 +48,48 @@ class PostDetail(APIView):
 
     def delete(self, request, pk, format=None):
         post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class PostComment(APIView):
+    def get(self, request, format=None):
+        comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class CommentDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            raise Http404
+
+
+    def get(self, request, pk , format=None):
+        post = self.get_object(pk)
+        serializer = CommentSerializer(post)
+        return Respnose(serializer.data)
+
+    def put(self, request, pk, format=None):
+        comments = self.get_object(pk)
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        comment = self.get_object(pk)
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
