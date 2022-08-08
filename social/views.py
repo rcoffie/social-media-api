a9f823b95dff1cf7e7ae6396f0949d8059ad2da9
Robert Coffie <rcoffie22@yhahoo.com>
2022-08-08 10:15:07 +0000

    complete crud of comment

--- social/views.py
+++ social/views.py
@@ -53,51 +53,6 @@ class PostDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class PostComment(APIView):
-    def get(self, request, format=None):
-        comments = Comment.objects.all()
-        serializer = CommentSerializer(comments, many=True)
-        return Response(serializer.data)
-
-
-
-    def post(self, request, format=None):
-        serializer = CommentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-class CommentDetail(APIView):
-
-    def get_object(self, pk):
-        try:
-            return Comment.objects.get(pk=pk)
-        except Comment.DoesNotExist:
-            raise Http404
-
-
-    def get(self, request, pk , format=None):
-        comment = self.get_object(pk)
-        serializer = CommentSerializer(post)
-        return Respnose(serializer.data)
-
-    def put(self, request, pk, format=None):
-        comments = self.get_object(pk)
-        serializer = CommentSerializer(comment, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        comment = self.get_object(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
 
 class Comments(generics.ListAPIView):
     queryset = Comment.objects.all()
@@ -113,3 +68,8 @@ class CreateComment(generics.CreateAPIView):
         pk = self.kwargs.get('pk')
         post = Post.objects.get(pk=pk)
         serializer.save(post=post)
+
+
+class UpdateComment(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
