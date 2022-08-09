9bbd317e086bb4b5a4d180010968111011610bcb
Robert Coffie <rcoffie22@yhahoo.com>
2022-08-09 14:29:48 +0000

    working on user_login

--- myso/urls.py
+++ myso/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('social.urls'))
+    path('',include('social.urls')),
+    path('user_app/',include('user_app.urls'))
 ]
