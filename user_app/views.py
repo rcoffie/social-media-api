6c7479d2a97dea188cc8ee569345aed8f7097496
Robert Coffie <rcoffie22@yhahoo.com>
2022-08-10 16:48:39 +0000

    working on user registgration

--- user_app/views.py
+++ user_app/views.py
@@ -3,11 +3,45 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.decorators import api_view , permission_classes
 from user_app.serializers import UserSerializer
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework import status
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework.authtoken.views import Token
+from django.contrib.auth import logout
 # Create your views here.
 
 
-class UserCreate(generics.ListCreateAPIView):
+class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+class UserRegistration(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            user = serializer.save()
+
+            data['username'] = user.username
+            data['email']  = user.email
+            token = Token.objects.get(user=user).key
+            data['token'] = token
+        else:
+            data = serializer.errors
+        return Response(data)
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def User_logout(request):
+
+    request.user.auth_token.delete()
+
+    logout(request)
+
+    return Response(status=status.HTTP_200_OK)
